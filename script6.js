ad741f853d7cc645d7015c766c5fb84e2ceaaea0
sergiopinelli1 <sergiopinelli202@gmail.com>
2025-06-15 18:56:19 -0300

    +

--- script6.js
+++ script6.js
@@ -209,81 +209,81 @@ document.addEventListener("DOMContentLoaded", function () {
           <tbody>
             <tr>
                 <td class="atributo2 titulo2">1</td>
-                <td>71</td>
+                <td>80</td>
+                <td>99</td>
+                <td>110</td>
+            </tr>
+            <tr>
+                <td class="atributo2 titulo2">5</td>
+                <td>74</td>
                 <td>88</td>
                 <td>98</td>
             </tr>
             <tr>
-                <td class="atributo2 titulo2">5</td>
-                <td>67</td>
-                <td>81</td>
-                <td>90</td>
-            </tr>
-            <tr>
                 <td class="atributo2 titulo2">10</td>
-                <td>64</td>
-                <td>77</td>
+                <td>70</td>
                 <td>82</td>
+                <td>91</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">20</td>
-                <td>45</td>
-                <td>70</td>
-                <td>79</td>
+                <td>51</td>
+                <td>78</td>
+                <td>87</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">30</td>
-                <td>32</td>
-                <td>64</td>
-                <td>76</td>
+                <td>41</td>
+                <td>69</td>
+                <td>79</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">40</td>
-                <td>28</td>
-                <td>57</td>
-                <td>71</td>
+                <td>38</td>
+                <td>65</td>
+                <td>81</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">50</td>
-                <td>25</td>
-                <td>49</td>
-                <td>65</td>
+                <td>31</td>
+                <td>56</td>
+                <td>75</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">60</td>
-                <td>20</td>
-                <td>45</td>
-                <td>59</td>
+                <td>28</td>
+                <td>55</td>
+                <td>64</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">70</td>
-                <td>18</td>
-                <td>39</td>
-                <td>56</td>
+                <td>24</td>
+                <td>48</td>
+                <td>62</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">80</td>
-                <td>17</td>
-                <td>34</td>
-                <td>49</td>
+                <td>23</td>
+                <td>43</td>
+                <td>57</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">90</td>
-                <td>16</td>
-                <td>29</td>
-                <td>44</td>
+                <td>20</td>
+                <td>40</td>
+                <td>54</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">100</td>
-                <td>15</td>
-                <td>26</td>
-                <td>41</td>
+                <td>18</td>
+                <td>36</td>
+                <td>48</td>
             </tr>
             <tr>
                 <td class="atributo2 titulo2">110</td>
-                <td>13</td>
-                <td>23</td>
-                <td>35</td>
+                <td>16</td>
+                <td>27</td>
+                <td>44</td>
             </tr>
         </tbody>
         </table>`,
